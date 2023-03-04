0525949ab4228d898652367eb1349013abc7259b
Gustavo Giuliano <61397269+JinsuGiuliano@users.noreply.github.com>
2023-03-04 01:13:42 -0300

    some css changes on header

--- src/components/menu/shortcuts/shortcuts.styles.tsx
+++ src/components/menu/shortcuts/shortcuts.styles.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { backgroundColor, textColor } from '../../../redux/theme/styles.const';
 
 export const ShortCutsContainer = styled.div`
     display: flex;
@@ -7,6 +8,19 @@ export const ShortCutsContainer = styled.div`
     justify-content:space-between;
     align-items: center;
     width:50%;
+    color: ${textColor};
+    @media screen and (max-width: 800px) {
+        flex:1;
+        float:center;
+        width:100%;
+        justify-content: space-around;
+        position: absolute;
+        bottom: -50%;
+        flex-wrap: nowrap;
+        overflow-x: auto;
+        background-color: ${backgroundColor};
+        z-index:99999;
+  }
 `
 
 export const WelcomeMessage = styled.div`
@@ -20,6 +34,12 @@ export const SectionSC = styled.div`
     align-items: center;
     padding-bottom: 5px;
     transition: padding-bottom 1000ms linear;
+
+    @media screen and (max-width: 800px) {
+         flex: 0 0 auto;
+    }
+ 
+
 `
 export const SectionSCLink = styled(Link)`
     font-size: 22px;
